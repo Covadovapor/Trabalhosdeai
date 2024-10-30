5b7a9f43e16dcc5f1cc7d386e84686d31073bd2c
Covadovapor <paivap677@gmail.com>
2024-10-30 09:06:43 +0000

    armazenar as ultimas 5 operações

--- calculadora.py
+++ calculadora.py
@@ -12,3 +12,10 @@ def divisao(x, y):
     return x / y
    else:
        return "Erro: Divisão por zero não é permitida"
+       
+# Função para armazenar as últimas 5 operações
+def armazenar_operacao(operacao, historico):
+    if len(historico) >=5:
+        historico.pop(0) #Remove a operação mais antiga
+    historico.append(operacao) #Adiciona a nova operação
+
